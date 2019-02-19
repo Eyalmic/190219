93295d9c369db9c99515bf0f0c252c3706002596
Eyalmic <47789454+Eyalmic@users.noreply.github.com>
2019-02-19 20:31:41 +0200

    Add files via upload

---
+++ Assigment2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Please enter 3 numbers to check for greatest:");
+			int a = Convert.ToInt32(Console.ReadLine());
+			int b = Convert.ToInt32(Console.ReadLine());
+			int c = Convert.ToInt32(Console.ReadLine());
+			if (a>b && a>c)
+				{
+				Console.WriteLine("The greatest number is:");
+				Console.WriteLine(a);
+				}
+			else if (b>c)
+				{
+				Console.WriteLine("The greatest number is:");
+				Console.WriteLine(b);
+				}
+			else
+				{
+				Console.WriteLine("The greatest number is:");
+				Console.WriteLine(c);
+				}
+
+		}
+	}
+}
