cc13e771b6e12c846eb2317f807043c1019c9c35
Eyalmic <47789454+Eyalmic@users.noreply.github.com>
2019-02-19 20:50:05 +0200

    Add files via upload

---
+++ 3numbersassigment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter 3 Numbers, if the multiplicative value of the first 2 is equal the 3rd you win!");
+			int a, b, c;
+			do
+			{
+				Console.WriteLine("Enter your numbers:");
+				a = Convert.ToInt32(Console.ReadLine());
+				b = Convert.ToInt32(Console.ReadLine());
+				c = Convert.ToInt32(Console.ReadLine());
+			}
+			while (a * b != c);
+			Console.WriteLine("you Win!");
+		}
+	}
+}
