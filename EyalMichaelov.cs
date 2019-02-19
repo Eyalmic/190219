1afaddb14d8b0d44bb278cfdd10ea7fc6c49c25b
Eyalmic <47789454+Eyalmic@users.noreply.github.com>
2019-02-19 19:28:04 +0200

    Add files via upload

---
+++ EyalMichaelov.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine("Please enter a number:");
+			int a = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Please enter an additional number to print greater:");
+			int b = Convert.ToInt32(Console.ReadLine());
+
+			if (a>b)
+				{
+				Console.WriteLine(a);
+				}
+			else
+				{
+				if (a == b)
+					{
+					Console.WriteLine("The numbers are even");
+					}
+				else
+					{
+					Console.WriteLine(b);
+					}
+				}
+
+		}
+	}
+}
