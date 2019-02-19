90f4633e70f0e59ba5e36a41af56933b44acb522
Eyalmic <47789454+Eyalmic@users.noreply.github.com>
2019-02-19 20:31:16 +0200

    Add files via upload

---
+++ AverageAssigment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double sum = 0;
+			int i = 0;
+			Console.WriteLine("Enter numbers to calculate Average (negative numbers will terminate the process):");
+
+			int x = Convert.ToInt32(Console.ReadLine());
+
+			while (x>=0)
+			{
+				sum = sum + x;
+				i++;
+				x = Convert.ToInt32(Console.ReadLine());
+			}
+			Console.WriteLine("The Average is:");
+			Console.WriteLine(sum/i);
+		}
+	}
+}
